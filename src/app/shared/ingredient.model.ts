b76a64c17f2b0b6b21316f7def0bc429f33cbac4
Suhail <suhailmoidin96@gmail.com>
2023-04-05 23:55:35 +0530

    new changes from macbook

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+export class Ingredient{
+    constructor(public name: string, public amount: number){
+
+    }
+}
+/*The above class visually works the same ways as an example below:
+export class Ingredients{
+    public name: string;
+    public amount: string;
+
+    constructor(name:string, amount:string){
+        this.name = name;
+        this.amount = amount;
+    }
+}
+*/
